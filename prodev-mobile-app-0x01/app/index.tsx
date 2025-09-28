afb5da5cdd878648f23f4dade784d212a567a147
Moeakae <ntsoakilekhoakhoa@gmail.com>
2025-09-28 18:11:38 +0200

    updated

--- prodev-mobile-app-0x01/app/index.tsx
+++ prodev-mobile-app-0x01/app/index.tsx
@@ -1,17 +1,29 @@
 import React from 'react';
 import { View, Text, StyleSheet } from 'react-native';
 
+
 const App = () => (
-  <View>
+  <View style={styles.container}>
+    {/* Main text */}
+    <Text style={styles.mainText}>Entry Screen - Awesome</Text>
     <Text style={styles.largeText}>Typescript is great if you practice more</Text>
     <Text style={styles.mediumText}>React Native provides you a single codebase for cross platforms</Text>
     <Text style={styles.smallText}>ALX is awesome</Text>
   </View>
 );
 
+
+
 const styles = StyleSheet.create({
   container: {
-    backgroundColor: "#90caf9",
+    flex: 1,                // Take full screen height
+    justifyContent: 'center', // Center content vertically
+    alignItems: 'center',     // Center content horizontally
+    backgroundColor: '#fff',  // White background
+  },
+  mainText: {
+    fontSize: 20,
+    fontWeight: 'bold',
   },
   largeText: {
     fontSize: 30,
@@ -34,8 +46,3 @@ const styles = StyleSheet.create({
     textAlign: "center",
   },
 });
-
-
-  
-
-export default App;
